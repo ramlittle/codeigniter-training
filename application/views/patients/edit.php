456abd8a1049f8eddb98c8408088ef18efca2138
ramelitomartinez <ramelitomartinez@gmail.com>
2025-03-26 10:53:54 +0800

    bugfix: made edit feature work

--- application/views/patients/edit.php
+++ application/views/patients/edit.php
@@ -7,8 +7,14 @@
 <h2>Edit Patient</h2>
 <?php echo validation_errors(); ?>
 <?php echo form_open('patient/edit/'.$patient->id); ?>
-<label for="name">Name:</label>
-<input type="text" name="name" value="<?php echo $patient->name; ?>"/>
+<label for="name">First Name</label>
+<input type="text" name="firstname" value="<?php echo $patient->firstname; ?>"/>
+
+<label for="name">Middle Name:</label>
+<input type="text" name="middlename" value="<?php echo $patient->middlename; ?>"/>
+
+<label for="name">Last Name:</label>
+<input type="text" name="lastname" value="<?php echo $patient->lastname; ?>"/>
 
 
 <label for="email">Email:</label>
@@ -18,6 +24,15 @@
 <label for="phone">Phone:</label>
 <input type="text" name="phone" value="<?php echo $patient->phone; ?>"/>
 
+<label>Birth Date:</label>
+<input type="date" name="birthdate" value="<?php echo $patient->birthdate ?>"><br>
+
+<label>Sex:</label>
+    <select name="sex">
+    <option value="">Select Sex</option>
+    <option value="M" <?php echo ($patient->sex == 'M') ? 'selected' : ''; ?>>Male</option>
+    <option value="F" <?php echo ($patient->sex == 'F') ? 'selected' : ''; ?>>Female</option>
+</select>
 
 <input type="submit" name="submit" value="Update Patient"/>
 <?php echo form_close(); ?>
