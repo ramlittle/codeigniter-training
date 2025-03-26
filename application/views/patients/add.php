2780334d799596a8b84de64756d9faafe733ee42
ramelitomartinez <ramelitomartinez@gmail.com>
2025-03-26 10:45:02 +0800

    bugfix: add patient

--- application/views/patients/add.php
+++ application/views/patients/add.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Add Patient</title>
 </head>
+
 <body>
     <h2>Add Patient</h2>
     <?php echo validation_errors(); ?>
     <?php echo form_open('patient/add'); ?>
 
-    <label>Name:</label>
-    <input type="text" name="name" value="<?php echo set_value('name'); ?>"><br>
+    <label>First Name:</label>
+    <input type="text" name="firstname" value="<?php echo set_value('firstname'); ?>"><br>
+    <label>Middle Name:</label>
+    <input type="text" name="middlename" value="<?php echo set_value('middlename'); ?>"><br>
+    <label>Last Name:</label>
+    <input type="text" name="lastname" value="<?php echo set_value('lastname'); ?>"><br>
 
     <label>Email:</label>
     <input type="text" name="email" value="<?php echo set_value('email'); ?>"><br>
@@ -17,8 +23,21 @@
     <label>Phone:</label>
     <input type="text" name="phone" value="<?php echo set_value('phone'); ?>"><br>
 
+    <label>Birth Date:</label>
+    <input type="date" name="birthdate" value="<?php echo set_value('birthdate'); ?>"><br>
+
+    <label>Sex:</label>
+    <select name="sex">
+        <option value="">Select Sex</option>
+        <option value="M"<?php echo set_select('sex','M'); ?>>Male</option>
+        <option value="F"<?php echo set_select('sex','F'); ?>>Female</option>
+    </select>
+    <br/>
     <button type="submit">Submit</button>
+
+    
 
     <?php echo form_close(); ?>
 </body>
+
 </html>
